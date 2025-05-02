63881a2b2e0b1e7daded257f78618dce70f17e7e
Hocine-ic <hocinehoho53@gmail.com>
2025-05-02 19:51:50 +0200

    simple message on backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
-// Middleware
-app.use(express.json());
+app.use(cors());
+
+// // Middleware
+// app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
-    res.send('Welcome to the backend server!');
+app.get('/api/home', (req, res) => {
+    res.json({ message: 'Welcome to the backend server!' })
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,29 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [message, setMessage] = useState("Loading...");
+  useEffect(() => {
+    
+    fetch('http://localhost:8000/api/home').then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log(data)
+        setMessage(data.message)
+      }
+    )
+  }, [])
+
   return (
     <div className="flex flex-col justify-center items-center  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1 className="text-4xl font-bold text-center sm:text-left">
           ChatZD
         </h1>
         <p className="text-lg text-center sm:text-left">
-          ChatZD is a chat application that allows you to communicate with
-          others in real-time.
+          {message}
         </p>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
