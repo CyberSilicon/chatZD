aca51bf2bd8211c06052a95b69dc0ec173d392c9
maki <maknaitammar@gmail.com>
2025-05-04 03:48:08 +0200

    add model profile

--- backend/src/models/profile.model.ts
+++ backend/src/models/profile.model.ts
@@ -1,1 +1,56 @@
-//hello
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 64,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxlength: 64,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    avatarUrl: {
+      type: String,
+      default: '',
+      validate: {
+        validator: function (v: any) {
+          return /^https?:\/\/.+/.test(v);
+        },
+        message: (props: any) => `${props.value} n'est pas une URL valide !`
+      }
+    },
+    privacyLastSeen: {
+      type: String,
+      enum: ['everyone', 'contacts', 'nobody'],
+      required: true,
+      default: 'everyone',
+    },
+    privacyPhoto: {
+      type: String,
+      enum: ['everyone', 'contacts', 'nobody'],
+      required: true,
+      default: 'everyone',
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('profile', profileSchema);
