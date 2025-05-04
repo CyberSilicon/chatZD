9cf3abbe5bf1f7e3dbc6587ecb438f9dcf127740
maki <maknaitammar@gmail.com>
2025-05-04 05:23:39 +0200

    updata and fix bug in user model 	modified:   backend/src/models/user.model.ts

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,12 +1,10 @@
-
-import { Schema, model, Document } from 'mongoose';
-import { IProfile, Profile } from './profile.model';
+import { Schema, model, Document, Types } from 'mongoose';
 
 export interface IUser extends Document {
   phoneNumber: string;
   username?: string;
   password: string;
-  profile: IProfile;
+  profile: Types.ObjectId;
   lastSeen: Date;
   isOnline: boolean;
   createdAt: Date;
@@ -17,7 +15,7 @@ const UserSchema = new Schema<IUser>(
     phoneNumber: { type: String, required: true, unique: true, trim: true, maxlength: 20 },
     username: { type: String, unique: true, sparse: true, trim: true, maxlength: 32 },
     password: { type: String, required: true },
-    profile: { type: Profile, required: false },
+    profile: { type: Schema.Types.ObjectId, ref: 'Profile', required: false },
     lastSeen: { type: Date, default: Date.now, index: true },
     isOnline: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now, immutable: true },
